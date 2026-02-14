ec7ca1e9efe88e48b699557720f9002ad4d329e5
sproshchaev <sproshchaev@gmail.com>
2026-02-14 14:48:34 +0500

    feat: add Wolf class

---
+++ src/main/java/com/javarush/forest/animal/Wolf.java
@@ -0,0 +1,47 @@
+package com.javarush.forest.animal;
+
+import com.javarush.forest.model.Location;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+public class Wolf extends Animal {
+
+    private static final Map<Class<? extends Animal>, Integer> EATING_PROBABILITIES = Map.of(
+            Rabbit.class, 60,
+            Deer.class, 80
+    );
+
+    public Wolf() {
+        super(50, 30); // вес 50 кг, максимальная сытость 30
+        this.eatingProbabilities = EATING_PROBABILITIES;
+    }
+
+    @Override
+    public void eat(Location location) {
+        if (!alive) return;
+        for (Animal prey : location.getAnimals()) {
+            if (prey == this || !prey.isAlive()) continue;
+            Integer prob = eatingProbabilities.get(prey.getClass());
+            if (prob != null && ThreadLocalRandom.current().nextInt(100) < prob) {
+                location.removeAnimal(prey);
+                prey.die();
+                currentSatiety = Math.min(maxSatiety, currentSatiety + prey.getWeight());
+                log.debug("Волк съел {}", prey.getClass().getSimpleName());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void move(Island island, int currentX, int currentY) {
+        // Пока заглушка
+    }
+
+    @Override
+    public void reproduce(Location location) {
+        // Пока заглушка
+    }
+}
