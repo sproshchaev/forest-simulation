b49b8bc253960ea20a3bedd90dc3b8b97b14affd
sproshchaev <sproshchaev@gmail.com>
2026-02-14 14:32:09 +0500

    feat: add abstract Animal class

---
+++ src/main/java/com/javarush/forest/animal/Animal.java
@@ -0,0 +1,38 @@
+package com.javarush.forest.animal;
+
+import com.javarush.forest.model.Location;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@Slf4j
+public abstract class Animal {
+    protected double weight;
+    protected double maxSatiety;
+    protected double currentSatiety;
+    protected boolean alive = true;
+    protected int speed = 1; // пока не используется
+
+    // Карта вероятностей поедания других животных (класс -> вероятность %)
+    protected Map<Class<? extends Animal>, Integer> eatingProbabilities;
+
+    public Animal(double weight, double maxSatiety) {
+        this.weight = weight;
+        this.maxSatiety = maxSatiety;
+        this.currentSatiety = maxSatiety;
+    }
+
+    public abstract void eat(Location location);
+
+    public abstract void move(Island island, int currentX, int currentY);
+
+    public abstract void reproduce(Location location);
+
+    public void die() {
+        this.alive = false;
+    }
+}
