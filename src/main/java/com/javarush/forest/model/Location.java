baee7fecd349d8ffb489dcc3c892fdd2efcb95ae
sproshchaev <sproshchaev@gmail.com>
2026-02-14 15:20:52 +0500

    Готово! Все работает.

--- src/main/java/com/javarush/forest/model/Location.java
+++ src/main/java/com/javarush/forest/model/Location.java
@@ -1,4 +1,34 @@
 package com.javarush.forest.model;
 
+import com.javarush.forest.animal.Animal;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
+    @Getter
+    private final List<Animal> animals = new ArrayList<>();
+    @Getter
+    private final List<Plant> plants = new ArrayList<>();
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    // В однопоточной версии удаление растения простое
+    public Plant removePlant() {
+        if (!plants.isEmpty()) {
+            return plants.remove(plants.size() - 1);
+        }
+        return null;
+    }
 }
