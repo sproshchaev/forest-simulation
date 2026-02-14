d53549dbb8678ae436e6b743c10704f416668cfe
sproshchaev <sproshchaev@gmail.com>
2026-02-14 14:33:21 +0500

    feat: add Island as grid of locations

---
+++ src/main/java/com/javarush/forest/model/Island.java
@@ -0,0 +1,29 @@
+package com.javarush.forest.model;
+
+import lombok.Getter;
+
+public class Island {
+    @Getter
+    private final int width;
+    @Getter
+    private final int height;
+    private final Location[][] locations;
+
+    public Island(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.locations = new Location[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                locations[i][j] = new Location();
+            }
+        }
+    }
+
+    public Location getLocation(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            throw new IllegalArgumentException("Координаты вне острова");
+        }
+        return locations[y][x];
+    }
+}
