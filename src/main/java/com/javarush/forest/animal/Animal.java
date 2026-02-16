fb34b3f5ab3607838f87707a36a961ee9ca1aa5d
sproshchaev <sproshchaev@gmail.com>
2026-02-16 21:29:41 +0500

    feat: add abstract Animal class

---
+++ src/main/java/com/javarush/forest/animal/Animal.java
@@ -0,0 +1,32 @@
+package com.javarush.forest.animal;
+
+import java.util.Map;
+
+public abstract class Animal {
+    protected double weight;
+    protected double maxSatiety; // максимальная сытость
+    protected double currentSatiety; // текущая сытость
+    protected boolean alive = true; // животное живое
+    protected int speed = 1; // скорость перемещения
+
+    // Карта вероятности поедания других животных
+    protected Map<Class<? extends Animal>, Integer> eatingProbabilities;
+
+    public Animal(double weight, double maxSatiety, double currentSatiety) {
+        this.weight = weight;
+        this.maxSatiety = maxSatiety;
+        this.currentSatiety = currentSatiety;
+    }
+
+    // eat, move, reproduce
+    public abstract void eat(Location location);
+
+    public abstract void move(Island island, int currentX, int currentY);
+
+    public abstract void reproduce(Location location);
+
+    public void die() {
+        this.alive = false;
+    }
+
+}
