f9aef41af314a047bd6dc81650fc680e2bd78d8a
sproshchaev <sproshchaev@gmail.com>
2026-02-19 20:06:43 +0500

    upd

--- src/main/java/com/javarush/forest/animal/Animal.java
+++ src/main/java/com/javarush/forest/animal/Animal.java
@@ -1,5 +1,8 @@
 package com.javarush.forest.animal;
 
+import com.javarush.forest.model.Island;
+import com.javarush.forest.model.Location;
+
 import java.util.Map;
 
 public abstract class Animal {
