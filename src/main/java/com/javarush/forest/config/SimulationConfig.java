8f4ca36b7a46453f81ef8d6e8ffb8e9b21325658
sproshchaev <sproshchaev@gmail.com>
2026-02-19 21:14:05 +0500

    feat: add SumulationConfig with builder

---
+++ src/main/java/com/javarush/forest/config/SimulationConfig.java
@@ -0,0 +1,25 @@
+package com.javarush.forest.config;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Конфигурация приложения
+ * Билдер позволяет создавать объекты в стиле SimulationConfig.builder().islandWidth(10)...build()
+ */
+@Data
+@Builder // аннотация из lombok
+public class SimulationConfig {
+    // Размеры Леса (Острова)
+    private int islandWidth;
+    private int islandHeight;
+    // Популяции
+    private int initialWolves;
+    private int initialRabbits;
+    private int initialDeer;
+    // Количество растений которые будут добавляться за 1 такт в каждую клетку
+    private int plantsPerCell;
+    // Дюрация в мс.
+    private long tickDurationMs;
+    // Вероятности
+}
