be1b7f89eb218cc8bd6aabe7e325cff56d685cb7
sproshchaev <sproshchaev@gmail.com>
2026-02-19 20:47:04 +0500

    feat: add Deer class

---
+++ src/main/java/com/javarush/forest/animal/Deer.java
@@ -0,0 +1,36 @@
+package com.javarush.forest.animal;
+
+import com.javarush.forest.model.Island;
+import com.javarush.forest.model.Location;
+import com.javarush.forest.model.Plant;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Deer - травоядное
+ */
+@Slf4j
+public class Deer extends Animal {
+
+
+    @Override
+    public void eat(Location location) {
+        if (!alive) {
+            return;
+        }
+        Plant plant = location.removePlant();
+        if (plant != null) {
+            currentSatiety = Math.min(maxSatiety, currentSatiety + plant.getWeight());
+            log.debug("Олень съел растение");
+        }
+    }
+
+    @Override
+    public void move(Island island, int currentX, int currentY) {
+        // todo пока заглушка
+    }
+
+    @Override
+    public void reproduce(Location location) {
+        // todo пока заглушка
+    }
+}
