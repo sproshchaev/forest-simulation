7c98f069de728cda945b3eed63344f9f105f80a6
sproshchaev <sproshchaev@gmail.com>
2026-02-19 20:35:18 +0500

    upd

--- src/main/java/com/javarush/forest/animal/Animal.java
+++ src/main/java/com/javarush/forest/animal/Animal.java
@@ -2,9 +2,15 @@ package com.javarush.forest.animal;
 
 import com.javarush.forest.model.Island;
 import com.javarush.forest.model.Location;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Data
+@NoArgsConstructor
+@Slf4j
 public abstract class Animal {
     protected double weight;
     protected double maxSatiety; // максимальная сытость
@@ -15,10 +21,10 @@ public abstract class Animal {
     // Карта вероятности поедания других животных
     protected Map<Class<? extends Animal>, Integer> eatingProbabilities;
 
-    public Animal(double weight, double maxSatiety, double currentSatiety) {
+    public Animal(double weight, double maxSatiety) {
         this.weight = weight;
         this.maxSatiety = maxSatiety;
-        this.currentSatiety = currentSatiety;
+        this.currentSatiety = maxSatiety;
     }
 
     // eat, move, reproduce
