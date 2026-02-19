9ae166e7149363bc7adcfcadbeb7a44f24d39fc3
sproshchaev <sproshchaev@gmail.com>
2026-02-19 20:35:55 +0500

    feat: add Wolf class

---
+++ src/main/java/com/javarush/forest/animal/Wolf.java
@@ -0,0 +1,53 @@
+package com.javarush.forest.animal;
+
+import com.javarush.forest.model.Island;
+import com.javarush.forest.model.Location;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Wolf хищник
+ */
+@Slf4j
+public class Wolf extends Animal {
+
+    private static final double WEIGTH = 50;
+    private static final double MAX_SATIETY = 30;
+    private static final Map<Class<? extends Animal>, Integer> EATING_PROBABILITIES = Map.of(); //todo добавить сущности Кролик и Олень
+
+    public Wolf() {
+        super(WEIGTH, MAX_SATIETY);
+        this.eatingProbabilities = EATING_PROBABILITIES;
+    }
+
+    @Override
+    public void eat(Location location) {
+        // Жив ли объект?
+        if (!alive) {
+            return;
+        }
+        for (Animal prey : location.getAnimals()) {
+            if (prey == this || !prey.isAlive()) continue;
+            Integer prob = eatingProbabilities.get(prey.getClass());
+            if (prob != null && ThreadLocalRandom.current().nextInt(100) < prob) {
+                location.removeAnimal(prey);
+                prey.die();
+                currentSatiety = Math.min(maxSatiety, currentSatiety + prey.getWeight());
+                log.debug("Волк съел {}", prey.getClass().getSimpleName());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void move(Island island, int currentX, int currentY) {
+        //todo Пока заглушка
+    }
+
+    @Override
+    public void reproduce(Location location) {
+        //todo Пока заглушка
+    }
+}
