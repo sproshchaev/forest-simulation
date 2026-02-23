f1b32f2c0baafbf3498941c12b6efec8bebc7f41
sproshchaev <sproshchaev@gmail.com>
2026-02-23 21:31:18 +0500

    feat: add currentLocation and move method

--- src/main/java/com/javarush/forest/animal/Animal.java
+++ src/main/java/com/javarush/forest/animal/Animal.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +18,7 @@ public abstract class Animal {
     protected double currentSatiety; // текущая сытость
     protected boolean alive = true; // животное живое
     protected int speed = 1; // скорость перемещения
+    protected volatile Location currentLocation; // текущее местонахождение животного
 
     // Карта вероятности поедания других животных
     protected Map<Class<? extends Animal>, Integer> eatingProbabilities;
@@ -30,7 +32,40 @@ public abstract class Animal {
     // eat, move, reproduce
     public abstract void eat(Location location);
 
-    public abstract void move(Island island, int currentX, int currentY);
+    public void move(Island island, int currentX, int currentY) {
+        if (!alive) {
+            return;
+        }
+        if (currentLocation == null) {
+            log.warn("Животное {} не имеет текущей локации. Перемещение не возможно!", this);
+            return;
+        }
+
+        int dirction = ThreadLocalRandom.current().nextInt(4);
+        int newX = currentX;
+        int newY = currentY;
+
+        // 0..3
+        switch (dirction) {
+            case 0:
+                // вверх Y
+                newY = Math.max(0, currentY - 1);
+                break;
+            case 1:
+                // вправо X
+                newX = Math.min(island.getWidth() - 1, currentX + 1);
+                break;
+            case 2:
+                // вниз Y
+                newY = Math.min(island.getHeight() - 1, currentY + 1);
+                break;
+            case 3:
+                // влево X
+                newX = Math.max(0, currentX - 1);
+                break;
+        }
+
+    }
 
     public abstract void reproduce(Location location);
 
